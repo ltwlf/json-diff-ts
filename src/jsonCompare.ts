f162a1732434728349035a953f8f875f2eccbdbd
Christian Glessner <cg@holospaces.com>
2021-02-09 12:09:09 +0100

    Fix lint issues

--- src/jsonCompare.ts
+++ src/jsonCompare.ts
@@ -1,21 +1,24 @@
-import { diff, flattenChangeset, getTypeOfObj, IFlatChange, Operation } from "./jsonDiff";
-import { keys, chain, replace, set } from "lodash"
+import { chain, keys, replace, set } from 'lodash';
+import { diff, flattenChangeset, getTypeOfObj, IFlatChange, Operation } from './jsonDiff';
 
 export enum CompareOperation {
-  CONTAINER = "CONTAINER",
-  UNCHANGED = "UNCHANGED"
+  CONTAINER = 'CONTAINER',
+  UNCHANGED = 'UNCHANGED'
 }
 
-export interface ComparisonEnrichedNode {
-  type: Operation | CompareOperation,
-  value: ComparisonEnrichedNode | ComparisonEnrichedNode[] | any | any[],
-  oldValue?: any
+export interface IComparisonEnrichedNode {
+  type: Operation | CompareOperation;
+  value: IComparisonEnrichedNode | IComparisonEnrichedNode[] | any | any[];
+  oldValue?: any;
 }
 
-export const createValue = (value: any): ComparisonEnrichedNode => ({ type: CompareOperation.UNCHANGED, value });
-export const createContainer = (value: object | []): ComparisonEnrichedNode => ({ type: CompareOperation.CONTAINER, value });
+export const createValue = (value: any): IComparisonEnrichedNode => ({ type: CompareOperation.UNCHANGED, value });
+export const createContainer = (value: object | []): IComparisonEnrichedNode => ({
+  type: CompareOperation.CONTAINER,
+  value
+});
 
-export const enrich = (object: any): ComparisonEnrichedNode => {
+export const enrich = (object: any): IComparisonEnrichedNode => {
   const objectType = getTypeOfObj(object);
 
   switch (objectType) {
@@ -23,58 +26,51 @@ export const enrich = (object: any): ComparisonEnrichedNode => {
       return keys(object)
         .map((key: string) => ({ key, value: enrich(object[key]) }))
         .reduce((accumulator, entry) => {
-          accumulator.value[entry.key] = entry.value
-          return accumulator
+          accumulator.value[entry.key] = entry.value;
+          return accumulator;
         }, createContainer({}));
     case 'Array':
       return chain(object)
         .map(value => enrich(value))
         .reduce((accumulator, value) => {
-          accumulator.value.push(value)
+          accumulator.value.push(value);
           return accumulator;
         }, createContainer([]))
-        .value()
+        .value();
     case 'Function':
       return undefined;
     case 'Date':
-    default: // Primitive value
+    default:
+      // Primitive value
       return createValue(object);
   }
 };
 
-export const applyChangelist = (object: ComparisonEnrichedNode, changelist: IFlatChange[]): ComparisonEnrichedNode => {
+export const applyChangelist = (object: IComparisonEnrichedNode, changelist: IFlatChange[]): IComparisonEnrichedNode => {
   chain(changelist)
-    .map(entry => ({ ...entry, path: replace(entry.path, "$.", ".") }))
-    .map(entry => ({ ...entry, path: replace(entry.path, /(\[(?<array>\d)\]\.)/g, "ARRVAL_START$<array>ARRVAL_END") }))
-    .map(entry => ({ ...entry, path: replace(entry.path, /(?<dot>\.)/g, ".value$<dot>") }))
-    .map(entry => ({ ...entry, path: replace(entry.path, /\./, "") }))
-    .map(entry => ({ ...entry, path: replace(entry.path, /ARRVAL_START/g, ".value[") }))
-    .map(entry => ({ ...entry, path: replace(entry.path, /ARRVAL_END/g, "].value.") }))
+    .map(entry => ({ ...entry, path: replace(entry.path, '$.', '.') }))
+    .map(entry => ({ ...entry, path: replace(entry.path, /(\[(?<array>\d)\]\.)/g, 'ARRVAL_START$<array>ARRVAL_END') }))
+    .map(entry => ({ ...entry, path: replace(entry.path, /(?<dot>\.)/g, '.value$<dot>') }))
+    .map(entry => ({ ...entry, path: replace(entry.path, /\./, '') }))
+    .map(entry => ({ ...entry, path: replace(entry.path, /ARRVAL_START/g, '.value[') }))
+    .map(entry => ({ ...entry, path: replace(entry.path, /ARRVAL_END/g, '].value.') }))
     .value()
     .forEach(entry => {
       switch (entry.type) {
         case Operation.ADD:
         case Operation.UPDATE:
-          set(
-            object,
-            entry.path,
-            { type: entry.type, value: entry.value, oldValue: entry.oldValue }
-          )
+          set(object, entry.path, { type: entry.type, value: entry.value, oldValue: entry.oldValue });
           break;
         case Operation.REMOVE:
-          set(
-            object,
-            entry.path,
-            { type: entry.type, value: undefined, oldValue: entry.value }
-          )
+          set(object, entry.path, { type: entry.type, value: undefined, oldValue: entry.value });
           break;
         default:
           throw new Error();
       }
-    })
+    });
   return object;
-}
+};
 
-export const compare = (oldObject: any, newObject: any): ComparisonEnrichedNode => {
+export const compare = (oldObject: any, newObject: any): IComparisonEnrichedNode => {
   return applyChangelist(enrich(oldObject), flattenChangeset(diff(oldObject, newObject)));
-}
+};

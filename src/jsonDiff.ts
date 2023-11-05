7ecaf9be5840cfca5d5bddb1fb2d823768a346b2
Christian Glessner <chris@leitwolf.io>
2023-11-05 11:45:43 +0000

    more code comments

--- src/jsonDiff.ts
+++ src/jsonDiff.ts
@@ -1,5 +1,267 @@
 import { difference, find, intersection, keyBy } from 'lodash';
 
+type FunctionKey = (obj: any, shouldReturnKeyName?: boolean) => any;
+export type EmbeddedObjKeysType = Record<string, string | FunctionKey>;
+export type EmbeddedObjKeysMapType = Map<string | RegExp, string | FunctionKey>;
+export enum Operation {
+  REMOVE = 'REMOVE',
+  ADD = 'ADD',
+  UPDATE = 'UPDATE'
+}
+
+export interface IChange {
+  type: Operation;
+  key: string;
+  embeddedKey?: string | FunctionKey;
+  value?: any | any[];
+  oldValue?: any;
+  changes?: IChange[];
+}
+export type Changeset = IChange[];
+
+export interface IFlatChange {
+  type: Operation;
+  key: string;
+  path: string;
+  valueType: string | null;
+  value?: any;
+  oldValue?: any;
+}
+
+export function diff(
+  oldObj: any,
+  newObj: any,
+  embeddedObjKeys?: EmbeddedObjKeysType | EmbeddedObjKeysMapType
+): IChange[] {
+  return compare(oldObj, newObj, [], embeddedObjKeys, []);
+}
+
+/**
+ * Applies all changes in the changeset to the object.
+ *
+ * @param {any} obj - The object to apply changes to.
+ * @param {Changeset} changeset - The changeset to apply.
+ * @returns {any} - The object after the changes from the changeset have been applied.
+ *
+ * The function first checks if a changeset is provided. If so, it iterates over each change in the changeset.
+ * If the change value is not null or undefined, or if the change type is REMOVE, it applies the change to the object directly.
+ * Otherwise, it applies the change to the corresponding branch of the object.
+ */
+export const applyChangeset = (obj: any, changeset: Changeset) => {
+  if (changeset) {
+    changeset.forEach((change) => {
+      const { type, key, value, embeddedKey } = change;
+
+      if ((value !== null && value !== undefined) || type === Operation.REMOVE) {
+        // Apply the change to the object
+        applyLeafChange(obj, change, embeddedKey);
+      } else {
+        // Apply the change to the branch
+        applyBranchChange(obj[key], change);
+      }
+    });
+  }
+  return obj;
+};
+
+/**
+ * Reverts the changes made to an object based on a given changeset.
+ *
+ * @param {any} obj - The object on which to revert changes.
+ * @param {Changeset} changeset - The changeset to revert.
+ * @returns {any} - The object after the changes from the changeset have been reverted.
+ *
+ * The function first checks if a changeset is provided. If so, it reverses the changeset to start reverting from the last change.
+ * It then iterates over each change in the changeset. If the change does not have any nested changes, it reverts the change on the object directly.
+ * If the change does have nested changes, it reverts the changes on the corresponding branch of the object.
+ */
+export const revertChangeset = (obj: any, changeset: Changeset) => {
+  if (changeset) {
+    changeset
+      .reverse()
+      .forEach((change: IChange): any =>
+        !change.changes ? revertLeafChange(obj, change) : revertBranchChange(obj[change.key], change)
+      );
+  }
+
+  return obj;
+};
+
+/**
+ * Flattens a changeset into an array of flat changes.
+ *
+ * @param {Changeset | IChange} obj - The changeset or change to flatten.
+ * @param {string} [path='$'] - The current path in the changeset.
+ * @param {string | FunctionKey} [embeddedKey] - The key to use for embedded objects.
+ * @returns {IFlatChange[]} - An array of flat changes.
+ *
+ * The function first checks if the input is an array. If so, it recursively flattens each change in the array.
+ * If the input is not an array, it checks if the change has nested changes or an embedded key.
+ * If so, it updates the path and recursively flattens the nested changes or the embedded object.
+ * If the change does not have nested changes or an embedded key, it creates a flat change and returns it in an array.
+ */
+export const flattenChangeset = (
+  obj: Changeset | IChange,
+  path = '$',
+  embeddedKey?: string | FunctionKey
+): IFlatChange[] => {
+  if (Array.isArray(obj)) {
+    return obj.reduce((memo, change) => [...memo, ...flattenChangeset(change, path, embeddedKey)], [] as IFlatChange[]);
+  } else {
+    if (obj.changes || embeddedKey) {
+      path = embeddedKey
+        ? embeddedKey === '$index'
+          ? `${path}[${obj.key}]`
+          : obj.type === Operation.ADD
+          ? path
+          : filterExpression(path, embeddedKey, obj.key)
+        : (path = append(path, obj.key));
+      return flattenChangeset(obj.changes || obj, path, obj.embeddedKey);
+    } else {
+      const valueType = getTypeOfObj(obj.value);
+      return [
+        {
+          ...obj,
+          path: valueType === 'Object' || path.endsWith(`[${obj.key}]`) ? path : append(path, obj.key),
+          valueType
+        }
+      ];
+    }
+  }
+};
+
+/**
+ * Transforms a flat changeset into a nested changeset.
+ *
+ * @param {IFlatChange | IFlatChange[]} changes - The flat changeset to unflatten.
+ * @returns {IChange[]} - The unflattened changeset.
+ *
+ * The function first checks if the input is a single change or an array of changes.
+ * It then iterates over each change and splits its path into segments.
+ * For each segment, it checks if it represents an array or a leaf node.
+ * If it represents an array, it creates a new change object and updates the pointer to this new object.
+ * If it represents a leaf node, it sets the key, type, value, and oldValue of the current change object.
+ * Finally, it pushes the unflattened change object into the changes array.
+ */
+export const unflattenChanges = (changes: IFlatChange | IFlatChange[]) => {
+  if (!Array.isArray(changes)) {
+    changes = [changes];
+  }
+
+  const changesArr: IChange[] = [];
+
+  changes.forEach((change) => {
+    const obj = {} as IChange;
+    let ptr = obj;
+
+    const segments = change.path.split(/([^@])\./).reduce((acc, curr, i) => {
+      const x = Math.floor(i / 2);
+      if (!acc[x]) {
+        acc[x] = '';
+      }
+      acc[x] += curr;
+      return acc;
+    }, []);
+
+    if (segments.length === 1) {
+      ptr.key = change.key;
+      ptr.type = change.type;
+      ptr.value = change.value;
+      ptr.oldValue = change.oldValue;
+      changesArr.push(ptr);
+    } else {
+      for (let i = 1; i < segments.length; i++) {
+        const segment = segments[i];
+        // Matches JSONPath segments: "items[?(@.id='123')]" or "items[2]"
+        const result = /^(.+)\[\?\(@\.(.+)='(.+)'\)]$|^(.+)\[(\d+)\]/.exec(segment);
+        // array
+        if (result) {
+          let key: string;
+          let embeddedKey: string;
+          let arrKey: string | number;
+          if (result[1]) {
+            key = result[1];
+            embeddedKey = result[2];
+            arrKey = result[3];
+          } else {
+            key = result[4];
+            embeddedKey = '$index';
+            arrKey = Number(result[5]);
+          }
+          // leaf
+          if (i === segments.length - 1) {
+            ptr.key = key!;
+            ptr.embeddedKey = embeddedKey!;
+            ptr.type = Operation.UPDATE;
+            ptr.changes = [
+              {
+                type: change.type,
+                key: arrKey!,
+                value: change.value,
+                oldValue: change.oldValue
+              } as IChange
+            ];
+          } else {
+            // object
+            ptr.key = key;
+            ptr.embeddedKey = embeddedKey;
+            ptr.type = Operation.UPDATE;
+            const newPtr = {} as IChange;
+            ptr.changes = [
+              {
+                type: Operation.UPDATE,
+                key: arrKey,
+                changes: [newPtr]
+              } as IChange
+            ];
+            ptr = newPtr;
+          }
+        } else {
+          // leaf
+          if (i === segments.length - 1) {
+            // check if value is a primitive or object
+            if (change.value !== null && change.valueType === 'Object') {
+              ptr.key = segment;
+              ptr.type = Operation.UPDATE;
+              ptr.changes = [
+                {
+                  key: change.key,
+                  type: change.type,
+                  value: change.value
+                } as IChange
+              ];
+            } else {
+              ptr.key = change.key;
+              ptr.type = change.type;
+              ptr.value = change.value;
+              ptr.oldValue = change.oldValue;
+            }
+          } else {
+            // branch
+            ptr.key = segment;
+            ptr.type = Operation.UPDATE;
+            const newPtr = {} as IChange;
+            ptr.changes = [newPtr];
+            ptr = newPtr;
+          }
+        }
+      }
+      changesArr.push(obj);
+    }
+  });
+  return changesArr;
+};
+
+/**
+ * Determines the type of a given object.
+ *
+ * @param {any} obj - The object whose type is to be determined.
+ * @returns {string | null} - The type of the object, or null if the object is null.
+ *
+ * This function first checks if the object is undefined or null, and returns 'undefined' or null respectively.
+ * If the object is neither undefined nor null, it uses Object.prototype.toString to get the object's type.
+ * The type is extracted from the string returned by Object.prototype.toString using a regular expression.
+ */
 export const getTypeOfObj = (obj: any) => {
   if (typeof obj === 'undefined') {
     return 'undefined';
@@ -9,6 +271,7 @@ export const getTypeOfObj = (obj: any) => {
     return null;
   }
 
+  // Extracts the "Type" from "[object Type]" string.
   return Object.prototype.toString.call(obj).match(/^\[object\s(.*)\]$/)[1];
 };
 
@@ -302,210 +565,6 @@ const revertBranchChange = (obj: any, change: any) => {
   } else {
     return revertChangeset(obj, change.changes);
   }
-};
-
-type FunctionKey = (obj: any, shouldReturnKeyName?: boolean) => any;
-export type EmbeddedObjKeysType = Record<string, string | FunctionKey>;
-export type EmbeddedObjKeysMapType = Map<string | RegExp, string | FunctionKey>;
-
-export function diff(
-  oldObj: any,
-  newObj: any,
-  embeddedObjKeys?: EmbeddedObjKeysType | EmbeddedObjKeysMapType
-): IChange[] {
-  return compare(oldObj, newObj, [], embeddedObjKeys, []);
-}
-
-export const applyChangeset = (obj: any, changeset: Changeset) => {
-  if (changeset) {
-    changeset.forEach((change) =>
-      (change.value !== null && change.value !== undefined) || change.type === Operation.REMOVE
-        ? applyLeafChange(obj, change, change.embeddedKey)
-        : applyBranchChange(obj[change.key], change)
-    );
-  }
-
-  return obj;
-};
-
-export const revertChangeset = (obj: any, changeset: Changeset) => {
-  if (changeset) {
-    changeset
-      .reverse()
-      .forEach((change: IChange): any =>
-        !change.changes ? revertLeafChange(obj, change) : revertBranchChange(obj[change.key], change)
-      );
-  }
-  return obj;
-};
-
-export enum Operation {
-  REMOVE = 'REMOVE',
-  ADD = 'ADD',
-  UPDATE = 'UPDATE'
-}
-
-export interface IChange {
-  type: Operation;
-  key: string;
-  embeddedKey?: string | FunctionKey;
-  value?: any | any[];
-  oldValue?: any;
-  changes?: IChange[];
-}
-export type Changeset = IChange[];
-
-export interface IFlatChange {
-  type: Operation;
-  key: string;
-  path: string;
-  valueType: string | null;
-  value?: any;
-  oldValue?: any;
-}
-
-export const flattenChangeset = (
-  obj: Changeset | IChange,
-  path = '$',
-  embeddedKey?: string | FunctionKey
-): IFlatChange[] => {
-  if (Array.isArray(obj)) {
-    return obj.reduce((memo, change) => [...memo, ...flattenChangeset(change, path, embeddedKey)], [] as IFlatChange[]);
-  } else {
-    if (obj.changes || embeddedKey) {
-      path = embeddedKey
-        ? embeddedKey === '$index'
-          ? `${path}[${obj.key}]`
-          : obj.type === Operation.ADD
-          ? path
-          : filterExpression(path, embeddedKey, obj.key)
-        : (path = append(path, obj.key));
-      return flattenChangeset(obj.changes || obj, path, obj.embeddedKey);
-    } else {
-      const valueType = getTypeOfObj(obj.value);
-      return [
-        {
-          ...obj,
-          path: valueType === 'Object' || path.endsWith(`[${obj.key}]`) ? path : append(path, obj.key),
-          valueType
-        }
-      ];
-    }
-  }
-};
-
-export const unflattenChanges = (changes: IFlatChange | IFlatChange[]) => {
-  if (!Array.isArray(changes)) {
-    changes = [changes];
-  }
-
-  const changesArr: IChange[] = [];
-
-  changes.forEach((change) => {
-    const obj = {} as IChange;
-    let ptr = obj;
-
-    const segments = change.path.split(/([^@])\./).reduce((acc, curr, i) => {
-      const x = Math.floor(i / 2);
-      if (!acc[x]) {
-        acc[x] = '';
-      }
-      acc[x] += curr;
-      return acc;
-    }, []);
-    // $.childern[@.name='chris'].age
-    // =>
-    // $
-    // childern[@.name='chris']
-    // age
-
-    if (segments.length === 1) {
-      ptr.key = change.key;
-      ptr.type = change.type;
-      ptr.value = change.value;
-      ptr.oldValue = change.oldValue;
-      changesArr.push(ptr);
-    } else {
-      for (let i = 1; i < segments.length; i++) {
-        const segment = segments[i];
-        // check for array
-        const result = /^(.+)\[\?\(@\.(.+)='(.+)'\)]$|^(.+)\[(\d+)\]/.exec(segment);
-        // array
-        if (result) {
-          let key: string;
-          let embeddedKey: string;
-          let arrKey: string | number;
-          if (result[1]) {
-            key = result[1];
-            embeddedKey = result[2];
-            arrKey = result[3];
-          } else {
-            key = result[4];
-            embeddedKey = '$index';
-            arrKey = Number(result[5]);
-          }
-          // leaf
-          if (i === segments.length - 1) {
-            ptr.key = key!;
-            ptr.embeddedKey = embeddedKey!;
-            ptr.type = Operation.UPDATE;
-            ptr.changes = [
-              {
-                type: change.type,
-                key: arrKey!,
-                value: change.value,
-                oldValue: change.oldValue
-              } as IChange
-            ];
-          } else {
-            // object
-            ptr.key = key;
-            ptr.embeddedKey = embeddedKey;
-            ptr.type = Operation.UPDATE;
-            const newPtr = {} as IChange;
-            ptr.changes = [
-              {
-                type: Operation.UPDATE,
-                key: arrKey,
-                changes: [newPtr]
-              } as IChange
-            ];
-            ptr = newPtr;
-          }
-        } else {
-          // leaf
-          if (i === segments.length - 1) {
-            // check if value is a primitive or object
-            if (change.value !== null && change.valueType === 'Object') {
-              ptr.key = segment;
-              ptr.type = Operation.UPDATE;
-              ptr.changes = [
-                {
-                  key: change.key,
-                  type: change.type,
-                  value: change.value
-                } as IChange
-              ];
-            } else {
-              ptr.key = change.key;
-              ptr.type = change.type;
-              ptr.value = change.value;
-              ptr.oldValue = change.oldValue;
-            }
-          } else {
-            // branch
-            ptr.key = segment;
-            ptr.type = Operation.UPDATE;
-            const newPtr = {} as IChange;
-            ptr.changes = [newPtr];
-            ptr = newPtr;
-          }
-        }
-      }
-      changesArr.push(obj);
-    }
-  });
-  return changesArr;
 };
 
 /** combine a base JSON Path with a subsequent segment */

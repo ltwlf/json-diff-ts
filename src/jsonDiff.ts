e2dd4329a49d811df34fd2f5c67f143d325dfc9f
Christian Glessner <chris@leitwolf.io>
2023-12-03 14:02:46 +0000

    Non greedy

--- src/jsonDiff.ts
+++ src/jsonDiff.ts
@@ -205,7 +205,7 @@ export const unflattenChanges = (changes: IFlatChange | IFlatChange[]) => {
       for (let i = 1; i < segments.length; i++) {
         const segment = segments[i];
         // Matches JSONPath segments: "items[?(@.id=='123')]", "items[?(@.id==123)]", "items[2]", "items[?(@='123')]"
-        const result = /^(.+)\[\?\(@.?([^=]*)?={1,2}'(.*)'\)\]$|^(.+)\[(\d+)\]$/.exec(segment); // NOSONAR
+        const result = /^(.+?)\[\?\(@.?([^=]*?)?={1,2}'(.*?)'\)\]$|^(.+?)\[(\d+?)\]$/.exec(segment);
         // array
         if (result) {
           let key: string;

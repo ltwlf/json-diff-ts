46a6f6bef7de9a92841dc6f0077a4c9f386b316b
Christian Glessner <chris@leitwolf.io>
2025-03-13 10:03:37 +0000

    Update ESLint configuration to new format

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module'
+      }
+    },
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off'
+    }
+  }
+];
